5621ff755bafd7fe32f9f9d8b5f6a38d44b8a3c2
Horizon <Horizon@HP2-PJ1720-C6>
2025-04-20 14:54:25 -0500

    Add project files.

---
+++ AccessControlSystem.h
@@ -0,0 +1,21 @@
+#ifndef ACCESS_CONTROL_SYSTEM_H
+#define ACCESS_CONTROL_SYSTEM_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+class ActionControlSystem {
+private:
+	string businessHours;
+
+public:
+	//Constructor
+	AccessControlSystem();
+
+	//Business hours getter
+	void setBusinessHours(const string& hours);
+	string getBusinessHours() const;
+
+};
